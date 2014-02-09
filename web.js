8334603792d2faeeaa945a1dbc3606f9f7d29036
observee <observee@gmail.com>
2014-02-09 16:37:16 +0000

    change index

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFile('index.html'),'utf-8');
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-})
+});
